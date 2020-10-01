e89acc6326e957c37acb95b31d9435793d3bca28
aakkat <64348307+aakkat@users.noreply.github.com>
2020-10-01 12:09:13 +0530

    dsl groovy

---
+++ dsl.groovy
@@ -0,0 +1,15 @@
+job("task6_job1"){
+description("The First Job: Downloading content from GitHub")
+        
+scm{
+github('aakkat/DevOpsTask6', 'master')
+}
+triggers {
+scm('* * * * *')
+}
+steps {
+shell('''rm -rvf /root/task3/*'''
+cp -rvf * /root/task3/
+)
+}
+}
